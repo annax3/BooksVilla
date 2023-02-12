f7123219aa9a47704bbe207b81149114d9be7527
Prashant Anand <906120anand@gmail.com>
2023-02-12 15:01:01 +0530

    PackagesCreated

---
+++ BooksVillaa/src/main/java/com/skillovilla/booksvillaa/BooksVillaaApplication.java
@@ -0,0 +1,13 @@
+package com.skillovilla.booksvillaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksVillaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksVillaaApplication.class, args);
+    }
+
+}
---
+++ BooksVillaa/src/test/java/com/skillovilla/booksvillaa/BooksVillaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillovilla.booksvillaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksVillaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
